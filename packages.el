3354f40d6f2028046a5aaa4cf0da12b5468aa727
Abey Onalaja <abideenonalaja@Abideens-Mac-Pro.local>
2019-07-10 00:06:51 +0100

    Project init

---
+++ packages.el
@@ -0,0 +1,24 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+(package! indium)
+(package! reason-mode)
+(package! prettier-js)
+(package! ob-typescript)
+(package! org-fancy-priorities)
+(package! move-text)
+(package! vue-mode)
+(package! origami)
+(package! org-super-agenda)
+(package! deadgrep)
+(package! dart-mode)
+(package! ob-dart)
+(package! kotlin-mode)
+(package! sql-indent)
+(package! org-brain)
+(package! ascii-art-to-unicode)
+(package! wakatime-mode)
